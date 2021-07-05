343f04117f02b1cd7a7e6606bb91c2c24c6367dd
Diego Alejandro Chávez Soria <diegochavezsoria@gmail.com>
2021-07-05 00:29:01 -0400

    feat: add tailwind support

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Nav extends Component {
   render () {
     return (
-      <nav className='nav'>
-        <ul>
-          <li>Home</li>
+      <nav>
+        <ul className='flex flex-row gap-10'>
+          <li className='ml-10'>Home</li>
           <li>New +</li>
           <li>Leaderboard</li>
         </ul>
