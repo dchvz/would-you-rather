657bc8ef4dd6bb3f52fef57c4d66f05e19460c89
Diego Alejandro Chávez Soria <diegochavezsoria@gmail.com>
2021-07-10 18:54:47 -0400

    add new color to taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cerise-red': {
+          '50': '#fef6f8',
+          '100': '#fdedf1',
+          '200': '#fbd1db',
+          '300': '#f9b5c5',
+          '400': '#f47e9a',
+          '500': '#ef476f',
+          '600': '#d74064',
+          '700': '#b33553',
+          '800': '#8f2b43',
+          '900': '#752336'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
