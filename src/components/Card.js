09f989ce174dedabba4844a5341f1ec3f6f0e3b4
Diego Alejandro Chávez Soria <diegochavezsoria@gmail.com>
2021-07-12 23:32:09 -0400

    refactor: use static height for the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,13 @@ class Card extends Component {
   render () {
     const { question, asker } = this.props
     return (
-      <div className="w-1/4 bg-gradient-to-b from-cerise-red-600 to-cerise-red-500 shadow-lg rounded-lg cursor-pointer">
-        <p className="text-white font-normal px-4 py-20">
-          {`Would you rather ${question.optionOne.text} OR ${question.optionTwo.text}`}
-        </p>
-        <div className="h-20 flex flex-wrap px-4 content-center gap-5 bg-white rounded-b-lg content-center">
+      <div className="w-1/4">
+        <div className="flex flex-wrap content-center h-64 bg-gradient-to-b from-cerise-red-600 to-cerise-red-500 shadow-lg rounded-t-lg cursor-pointer">
+          <p className="text-white font-normal px-4 my-auto">
+            {`Would you rather ${question.optionOne.text} OR ${question.optionTwo.text}?`}
+          </p>
+        </div>
+        <div className="h-20 flex flex-wrap shadow-lg px-4 content-center gap-5 bg-white rounded-b-lg content-center">
           <Avatar avatarURL={asker.avatarURL} dimensions={'h-12 w-12'}/>
           <div>
             <p className="text-gray-700 text-sm font-extralight my-auto text-left">Asked by</p>
